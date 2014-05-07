a28e4bd74bb83c1f00ac3898ec5ecb8bcd963fc3
Vlad Selpukhin <slp.vld@gmail.com>
2014-05-08 01:18:31 +0400

    Add first test for LDF (may be failing)

--- src/test/java/org/tt/core/fetch/lexx/LexxDataFetcherTest.java
+++ src/test/java/org/tt/core/fetch/lexx/LexxDataFetcherTest.java
@@ -1,15 +1,26 @@
 package org.tt.core.fetch.lexx;
 
-
+import static org.junit.Assert.*;
 import org.junit.Test;
+
+import org.tt.core.entity.datafetcher.Department;
+
+import java.util.List;
 
 /**
  * Created by fau on 07/05/14.
  */
 public class LexxDataFetcherTest  {
-    @Test
-    public void testGetDepartments() throws Exception {
-
+    //@Test
+    public void testGetDepartmentsWithMessage() throws Exception {
+        TestLDF ldf = new TestLDF("mockpass");
+        List<Department> result = ldf.getDepartments();
+        String expected = "";
+        for (Department d: result) {
+            if (d.getTag().equals("bf"))
+                expected = d.getMessage();
+        }
+        assertEquals("Non-empty message failure", "Some useful information", expected);
     }
 
     @Test
