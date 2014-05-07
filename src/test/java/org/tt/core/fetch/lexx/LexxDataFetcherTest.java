7f29a8ad09b884091f1a29ffd27359e2dd40879b
Vlad Selpukhin <slp.vld@gmail.com>
2014-05-07 13:14:35 +0400

    Add test workarounds

---
+++ src/test/java/org/tt/core/fetch/lexx/LexxDataFetcherTest.java
@@ -0,0 +1,20 @@
+package org.tt.core.fetch.lexx;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by fau on 07/05/14.
+ */
+public class LexxDataFetcherTest extends TestCase {
+    public void testGetDepartments() throws Exception {
+
+    }
+
+    public void testGetGroups() throws Exception {
+
+    }
+
+    public void testGetTT() throws Exception {
+
+    }
+}
---
+++ src/test/java/org/tt/core/sql/SSUSQLManagerTest.java
@@ -0,0 +1,100 @@
+package org.tt.core.sql;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by fau on 07/05/14.
+ */
+public class SSUSQLManagerTest extends TestCase {
+    public void testPutDepartment() throws Exception {
+
+    }
+
+    public void testPutGroup() throws Exception {
+
+    }
+
+    public void testPutDateTime() throws Exception {
+
+    }
+
+    public void testPutSubject() throws Exception {
+
+    }
+
+    public void testPutLocation() throws Exception {
+
+    }
+
+    public void testPutTeacher() throws Exception {
+
+    }
+
+    public void testPutSubGroup() throws Exception {
+
+    }
+
+    public void testPutLessonRecord() throws Exception {
+
+    }
+
+    public void testUpdateDepartmentMessage() throws Exception {
+
+    }
+
+    public void testUpdateDepartmentInfo() throws Exception {
+
+    }
+
+    public void testGetDepartments() throws Exception {
+
+    }
+
+    public void testGetDepartmentTags() throws Exception {
+
+    }
+
+    public void testGetDepartmentMessage() throws Exception {
+
+    }
+
+    public void testGetGroups() throws Exception {
+
+    }
+
+    public void testGetNonEmptyGroups() throws Exception {
+
+    }
+
+    public void testGetTT() throws Exception {
+
+    }
+
+    public void testGetLessonList() throws Exception {
+
+    }
+
+    public void testDeleteDepartment() throws Exception {
+
+    }
+
+    public void testDeleteGroupFromDepartment() throws Exception {
+
+    }
+
+    public void testDeleteLessons() throws Exception {
+
+    }
+
+    public void testDeleteSubGroups() throws Exception {
+
+    }
+
+    public void testDeleteLesson() throws Exception {
+
+    }
+
+    public void testFlushDatabase() throws Exception {
+
+    }
+}
