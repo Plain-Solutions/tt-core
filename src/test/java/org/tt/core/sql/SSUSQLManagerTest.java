638e3a82d5d9b04676983a27b0c035f2d627ba2a
Vlad Selpukhin <slp.vld@gmail.com>
2014-05-10 02:26:51 +0400

    Introduce DbUnit and first DB test

--- src/test/java/org/tt/core/sql/SSUSQLManagerTest.java
+++ src/test/java/org/tt/core/sql/SSUSQLManagerTest.java
@@ -1,50 +1,61 @@
+/*
+* Copyright 2014 Plain Solutions
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.tt.core.sql;
 
+import edu.emory.mathcs.backport.java.util.Arrays;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.RunScript;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.tt.core.entity.datafetcher.Department;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 
-/**
- * Created by fau on 07/05/14.
- */
 public class SSUSQLManagerTest {
-    @Test
-    public void testPutDepartment() throws Exception {
+    private static final String JDBCDRIVER = org.h2.Driver.class.getName();
+    private static final String JDBCPATH = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String DBLOGIN = "sa";
+    private static final String DBPASS = "";
+    private static final String DBSCHEME = "./src/test/resources/initTT.sql";
+    private static final String DBDATASET = "./src/test/resources/dataset.xml";
+    private static final Charset DBCS = StandardCharsets.UTF_8;
 
+    @BeforeClass
+    public static void createSchema() throws Exception {
+        RunScript.execute(JDBCPATH, DBLOGIN, DBPASS, DBSCHEME, DBCS, false);
     }
 
-    @Test
-    public void testPutGroup() throws Exception {
-
-    }
-
-    @Test
-    public void testPutDateTime() throws Exception {
-
-    }
-
-    @Test
-    public void testPutSubject() throws Exception {
-
-    }
-
-    @Test
-    public void testPutLocation() throws Exception {
-
-    }
-
-    @Test
-    public void testPutTeacher() throws Exception {
-
-    }
-
-    @Test
-    public void testPutSubGroup() throws Exception {
-
-    }
-
-    @Test
-    public void testPutLessonRecord() throws Exception {
-
+    @Before
+    public void importDataSet() throws Exception {
+        IDataSet dataSet = readDataSet();
+        cleanlyInsert(dataSet);
     }
 
     @Test
@@ -59,6 +70,16 @@ public class SSUSQLManagerTest {
 
     @Test
     public void testGetDepartments() throws Exception {
+        AbstractSQLManager sqlm = new SSUSQLManager(getConnection(), new H2Queries());
+        List<Department> result = sqlm.getDepartments();
+        List<Department> expected = java.util.Arrays.asList(new Department("Biology House", "bf", "Crucial Information!"),
+                new Department("Geography House", "gf", ""));
+
+        assertTrue("Departments getting failed" +
+                        "\n  'result'        = " + result +
+                        "\n  'expected' = " + expected,
+                expected.equals(result)
+        );
 
     }
 
@@ -121,4 +142,32 @@ public class SSUSQLManagerTest {
     public void testFlushDatabase() throws Exception {
 
     }
+
+    private Connection getConnection() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL(JDBCPATH);
+        dataSource.setUser(DBLOGIN);
+        dataSource.setPassword(DBPASS);
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void cleanlyInsert(IDataSet dataSet) throws Exception {
+        IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBCDRIVER, JDBCPATH, DBLOGIN, DBPASS);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
+    }
+
+
+    private IDataSet readDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(new File(DBDATASET));
+    }
+
+
+
 }
